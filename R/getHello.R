896647ce8923795f4aa17e5d94cd48e281adff81
ViriginiaPCS <lchen@ecsu.edu>
2018-11-28 13:05:00 -0500

    R Package for Github

---
+++ R/getHello.R
@@ -0,0 +1,14 @@
+#' getHello Function
+#'
+#' print "Hello World!" to the std
+#' @author Lin Chen, \email{lin.chen@ieee.org}
+#' @references \url{https://lin-chen-va.github.io/}
+#' @seealso \code{\link{getRand}}
+#' @keywords print
+#' @export
+#' @examples
+#' getHello()
+getHello <- function()
+{
+	print("Hello World!")
+}
---
+++ R/getRand.R
@@ -0,0 +1,14 @@
+#' getRand Function
+#'
+#' get a sequence of random numbers between 0 and 1
+#' @author Lin Chen, \email{lin.chen@ieee.org}
+#' @references \url{https://lin-chen-va.github.io/}
+#' @param n the number of random numbers
+#' @keywords cats
+#' @export
+#' @examples
+#' v = getRand(10)
+getRand <- function(n)
+{
+	return(runif(n))
+}
